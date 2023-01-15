2e8611153f21f62470bc716411e4175be73e2d75
Tony <tonylyon0007@gmail.com>
2023-01-15 17:12:14 +0100

    hello

--- server.js
+++ server.js
@@ -47,9 +47,13 @@ app.get("/home", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
-);
+// app.listen(
+//     PORT,
+//     console.log(
+//         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+//     )
+// );
+
+app.listen (PORT, () => {
+    console.log(`server running in ${process.env.NODE_ENV} node on port ${PORT}`)
+  })
