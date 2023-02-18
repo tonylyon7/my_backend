f44e919e09c86eb174f6c3382fa94950730226d0
Tony <tonylyon0007@gmail.com>
2023-02-18 21:24:35 +0100

    Hi

--- Validations/UserValidation.js
+++ Validations/UserValidation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 import validateRequest from './validate.js';
 
-const strongPasswordRegex = /^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$%^&*-]).{8,}$/;
+const strongPasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 const stringPassswordError = 'Password must be strong. At least one upper case alphabet. At least one lower case alphabet. At least one digit. At least one special character. Minimum eight in length';
 
 export const signup = (req, res, next) => { 
