a047be547d60b4be2daa18ac0096851582a8470f
Tony <tonylyon0007@gmail.com>
2023-06-19 07:12:49 +0100

    Hello

--- utilities/generate_token.js
+++ utilities/generate_token.js
@@ -1,8 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET,{
+    return jwt.sign({id}, "sjnuenbcjsdubeubcsdjfrgjrtlgjrtgmkkltmilk",{
         expiresIn: "1d"
     })
 }
+
+// Heroku
+// const generateToken = (id) => {
+//     return jwt.sign({id}, process.env.JWT_SECRET,{
+//         expiresIn: "1d"
+//     })
+// }
 export {generateToken}
